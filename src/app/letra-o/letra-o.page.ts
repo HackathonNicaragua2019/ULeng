c7ae19677220ed2de52c1a65690530ca445c6a42
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 08:49:25 -0600

    refactore: Se modificó el o.page

--- src/app/letra-o/letra-o.page.ts
+++ src/app/letra-o/letra-o.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-letra-o',
   templateUrl: './letra-o.page.html',
   styleUrls: ['./letra-o.page.scss'],
 })
-export class LetraOPage implements OnInit {
+export class LetraOPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  home(){
+    this.router.navigate(["/categorias"]);
+  }
+
+  regresar_categoria(){
+    this.router.navigate(["/cat-abecedario"]);
+  }
+
+  siguiente(){
+    this.router.navigate(["/letra-p"]);
+  }
+
+  anterior(){
+    this.router.navigate(["/letra-n1"]);
+  }
+
+  public ocultar: boolean = false;
+  accion(){
+    this.ocultar = !this.ocultar;
   }
 
 }
