8369153debbbbcd3bf08916e3e8484132568cf7c
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 04:35:09 -0600

    refactor: Se modificó el componente auth.guard.ts

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements  {
-  
+export class AuthGuard implements CanActivate {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable <boolean | UrlTree> | 
+    Promise<boolean | UrlTree> | boolean | UrlTree {
+      return true;
+    }
 }
