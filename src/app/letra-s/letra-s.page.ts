c2bd0d3ca74014cbc8c6ff7b020b91438f5d7247
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 09:12:37 -0600

    refactore: Se modifico el s.page

--- src/app/letra-s/letra-s.page.ts
+++ src/app/letra-s/letra-s.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-letra-s',
   templateUrl: './letra-s.page.html',
   styleUrls: ['./letra-s.page.scss'],
 })
-export class LetraSPage implements OnInit {
+export class LetraSPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  home(){
+    this.router.navigate(["/categorias"]);
+  }
+
+  regresar_categoria(){
+    this.router.navigate(["/cat-abecedario"]);
+  }
+
+  siguiente(){
+    this.router.navigate(["/letra-t"]);
+  }
+
+  anterior(){
+    this.router.navigate(["/letra-rr"]);
+  }
+
+  public ocultar: boolean = false;
+  accion(){
+    this.ocultar = !this.ocultar;
   }
 
 }
