5502f594e90f54bcaf5ce12fd2fb87da18777ebb
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 07:28:04 -0600

    refactor: Se modific[o el g.page

--- src/app/letra-g/letra-g.page.ts
+++ src/app/letra-g/letra-g.page.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-letra-g',
   templateUrl: './letra-g.page.html',
   styleUrls: ['./letra-g.page.scss'],
 })
-export class LetraGPage implements OnInit {
+export class LetraGPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  home(){
+    this.router.navigate(["/categorias"]);
   }
+
+  regresar_categoria(){
+    this.router.navigate(["/cat-abecedario"]);
+  }
+
+  siguiente(){
+    this.router.navigate(["/letra-h"]);
+  }
+
+  anterior(){
+    this.router.navigate(["/letra-f"]);
+  }
+
+  public ocultar: boolean = false;
+  accion(){
+    this.ocultar = !this.ocultar;
+  }
+
 
 }
