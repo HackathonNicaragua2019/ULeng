7992e8560086e8b4c3124576d416848ab29b9f75
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 08:33:09 -0600

    refactore: Se modificó la m.page

--- src/app/letra-m/letra-m.page.ts
+++ src/app/letra-m/letra-m.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-letra-m',
   templateUrl: './letra-m.page.html',
   styleUrls: ['./letra-m.page.scss'],
 })
-export class LetraMPage implements OnInit {
+export class LetraMPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  home(){
+    this.router.navigate(["/categorias"]);
+  }
+
+  regresar_categoria(){
+    this.router.navigate(["/cat-abecedario"]);
+  }
+
+  siguiente(){
+    this.router.navigate(["/letra-n"]);
+  }
+
+  anterior(){
+    this.router.navigate(["/letra-ll"]);
+  }
+
+  public ocultar: boolean = false;
+  accion(){
+    this.ocultar = !this.ocultar;
   }
 
 }
