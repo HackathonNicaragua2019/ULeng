1274f8bb7528d73edffd5ed135f05ac345e06286
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 07:20:38 -0600

    refactor: Se modificó los componentes de f.page

--- src/app/letra-f/letra-f.page.ts
+++ src/app/letra-f/letra-f.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-letra-f',
   templateUrl: './letra-f.page.html',
   styleUrls: ['./letra-f.page.scss'],
 })
-export class LetraFPage implements OnInit {
+export class LetraFPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  home(){
+    this.router.navigate(["/categorias"]);
+  }
+
+  regresar_categoria(){
+    this.router.navigate(["/cat-abecedario"]);
+  }
+
+  siguiente(){
+    this.router.navigate(["/letra-g"]);
+  }
+
+  anterior(){
+    this.router.navigate(["/letra-e"]);
+  }
+
+  public ocultar: boolean = false;
+  accion(){
+    this.ocultar = !this.ocultar;
   }
 
 }
