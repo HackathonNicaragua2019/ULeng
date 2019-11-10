6e88a93a910e4015e77f7c3d3edb6a75528ca9c2
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 09:50:20 -0600

    refactore: se modificó el z.page

--- src/app/letra-z/letra-z.page.ts
+++ src/app/letra-z/letra-z.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-letra-z',
   templateUrl: './letra-z.page.html',
   styleUrls: ['./letra-z.page.scss'],
 })
-export class LetraZPage implements OnInit {
+export class LetraZPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  home(){
+    this.router.navigate(["/categorias"]);
+  }
+
+  regresar_categoria(){
+    this.router.navigate(["/cat-abecedario"]);
+  }
+
+  anterior(){
+    this.router.navigate(["/letra-y"]);
+  }
+
+  public ocultar: boolean = false;
+  accion(){
+    this.ocultar = !this.ocultar;
   }
 
 }
