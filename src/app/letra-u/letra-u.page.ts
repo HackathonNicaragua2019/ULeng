0331259742f729dd1bf026d304f5da82dfc795b7
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 09:23:41 -0600

    refactore: Se modificó u.page

--- src/app/letra-u/letra-u.page.ts
+++ src/app/letra-u/letra-u.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-letra-u',
   templateUrl: './letra-u.page.html',
   styleUrls: ['./letra-u.page.scss'],
 })
-export class LetraUPage implements OnInit {
+export class LetraUPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  home(){
+    this.router.navigate(["/categorias"]);
+  }
+
+  regresar_categoria(){
+    this.router.navigate(["/cat-abecedario"]);
+  }
+
+  siguiente(){
+    this.router.navigate(["/letra-v"]);
+  }
+
+  anterior(){
+    this.router.navigate(["/letra-t"]);
+  }
+
+  public ocultar: boolean = false;
+  accion(){
+    this.ocultar = !this.ocultar;
   }
 
 }
