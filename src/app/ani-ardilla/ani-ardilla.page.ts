fdfbf799173b2912e53bdf48b25a8d57a776a8be
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 10:38:12 -0600

    refactore: Se modifico el ani-ardilla.page

--- src/app/ani-ardilla/ani-ardilla.page.ts
+++ src/app/ani-ardilla/ani-ardilla.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ani-ardilla',
   templateUrl: './ani-ardilla.page.html',
   styleUrls: ['./ani-ardilla.page.scss'],
 })
-export class AniArdillaPage implements OnInit {
+export class AniArdillaPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  home(){
+    this.router.navigate(["/categorias"]);
+  }
+
+  regresar_categoria(){
+    this.router.navigate(["/cat-animales"]);
+  }
+
+  siguiente(){
+    this.router.navigate(["/ani-caballo"]);
+  }
+
+  public ocultar: boolean = false;
+  accion(){
+    this.ocultar = !this.ocultar;
   }
 
 }
