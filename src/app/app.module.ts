eef8e9d0a4c7568ed1fbb795b4f8e11b2f3bc035
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 03:04:50 -0600

    feat: se modificó app.module.ts y auth.service.ts y se creó una nueva carpeta services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule, AngularFireAuth } from '@angular/fire/auth';
+import { enviroment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(enviroment.firebaseConfig),
+    AngularFireAuthModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { async } from 'q';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  public isLogged: any = false;
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => (this.isLogged = user))
+  }
+
+  // login
+  async onLogin (user) {
+    try{
+      return await this.afAuth.auth.signInWithEmailAndPassword(
+        user.email, 
+        user.password
+      );
+    } catch (error) {
+      console.log('Error on login', error);
+    }
+  }
+
+  //registro
+  async onRegister(user){
+    try{
+      return await this.afAuth.auth.createUserWithEmailAndPassword(
+        user.email,
+        user.password
+      );
+    } catch (error){
+      console.log('Error on register user', error);
+    }
+  }
 }
