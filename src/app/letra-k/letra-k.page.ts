ffb6cf4917654b5c1499d1d791feda2bfcc158e9
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 07:59:38 -0600

    refactor: Se modificó el k.page

--- src/app/letra-k/letra-k.page.ts
+++ src/app/letra-k/letra-k.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-letra-k',
   templateUrl: './letra-k.page.html',
   styleUrls: ['./letra-k.page.scss'],
 })
-export class LetraKPage implements OnInit {
+export class LetraKPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  home(){
+    this.router.navigate(["/categorias"]);
+  }
+
+  regresar_categoria(){
+    this.router.navigate(["/cat-abecedario"]);
+  }
+
+  siguiente(){
+    this.router.navigate(["/letra-l"]);
+  }
+
+  anterior(){
+    this.router.navigate(["/letra-j"]);
+  }
+
+  public ocultar: boolean = false;
+  accion(){
+    this.ocultar = !this.ocultar;
   }
 
 }
