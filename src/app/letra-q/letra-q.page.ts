92b5ee7c3abd8863409c94500c2d4994c48123c1
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 08:58:40 -0600

    refactore: se modificó el q.page

--- src/app/letra-q/letra-q.page.ts
+++ src/app/letra-q/letra-q.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-letra-q',
   templateUrl: './letra-q.page.html',
   styleUrls: ['./letra-q.page.scss'],
 })
-export class LetraQPage implements OnInit {
+export class LetraQPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  home(){
+    this.router.navigate(["/categorias"]);
+  }
+
+  regresar_categoria(){
+    this.router.navigate(["/cat-abecedario"]);
+  }
+
+  siguiente(){
+    this.router.navigate(["/letra-r"]);
+  }
+
+  anterior(){
+    this.router.navigate(["/letra-p"]);
+  }
+
+  public ocultar: boolean = false;
+  accion(){
+    this.ocultar = !this.ocultar;
   }
 
 }
