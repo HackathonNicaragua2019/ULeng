77fa36e960873d961f5d984536445f65dece7aa7
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 00:48:41 -0600

    docs: modificacion de pantalla

--- src/app/letra-a/letra-a.page.ts
+++ src/app/letra-a/letra-a.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-letra-a',
   templateUrl: './letra-a.page.html',
   styleUrls: ['./letra-a.page.scss'],
 })
-export class LetraAPage implements OnInit {
+export class LetraAPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  home(){
+    this.router.navigate(["/home"]);
+  }
+
+  regresar_categoria(){
+    this.router.navigate(["/categorias"]);
+  }
+
+  siguiente(){
+    this.router.navigate(["/letra-b"]);
+  }
+
+  public ocultar: boolean = false;
+  accion(){
+    this.ocultar = !this.ocultar;
   }
 
 }
