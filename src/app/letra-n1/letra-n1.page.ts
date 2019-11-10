ed2a92eaf89547b911d747331f708aa517e65865
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 08:44:07 -0600

    refactore: Se modificó el n1.page

--- src/app/letra-n1/letra-n1.page.ts
+++ src/app/letra-n1/letra-n1.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-letra-n1',
   templateUrl: './letra-n1.page.html',
   styleUrls: ['./letra-n1.page.scss'],
 })
-export class LetraN1Page implements OnInit {
+export class LetraN1Page {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  home(){
+    this.router.navigate(["/categorias"]);
+  }
+
+  regresar_categoria(){
+    this.router.navigate(["/cat-abecedario"]);
+  }
+
+  siguiente(){
+    this.router.navigate(["/letra-o"]);
+  }
+
+  anterior(){
+    this.router.navigate(["/letra-n"]);
+  }
+
+  public ocultar: boolean = false;
+  accion(){
+    this.ocultar = !this.ocultar;
   }
 
 }
