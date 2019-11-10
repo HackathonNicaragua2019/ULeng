49bb4cad7762eaf78654cf983bb6dbaa5f6bf285
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 06:49:36 -0600

    refactor: Se modificaron los componentes de letra-b.page

--- src/app/letra-b/letra-b.page.ts
+++ src/app/letra-b/letra-b.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-letra-b',
   templateUrl: './letra-b.page.html',
   styleUrls: ['./letra-b.page.scss'],
 })
-export class LetraBPage implements OnInit {
+export class LetraBPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  home(){
+    this.router.navigate(["/categorias"]);
   }
 
+  regresar_categoria(){
+    this.router.navigate(["/cat-abecedario"]);
+  }
+
+  siguiente(){
+    this.router.navigate(["/letra-c"]);
+  }
+
+  anterior(){
+    this.router.navigate(["/letra-a"]);
+  }
+
+  public ocultar: boolean = false;
+  accion(){
+    this.ocultar = !this.ocultar;
+  }
 }
