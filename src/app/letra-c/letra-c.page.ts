b02f2ddd5a1bcbfce3ab3f7e6b00666ed73da957
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 07:01:35 -0600

    refactor: Se modificaron los c.page y ch.page

--- src/app/letra-c/letra-c.page.ts
+++ src/app/letra-c/letra-c.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-letra-c',
   templateUrl: './letra-c.page.html',
   styleUrls: ['./letra-c.page.scss'],
 })
-export class LetraCPage implements OnInit {
+export class LetraCPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  home(){
+    this.router.navigate(["/categorias"]);
   }
 
+  regresar_categoria(){
+    this.router.navigate(["/cat-abecedario"]);
+  }
+
+  siguiente(){
+    this.router.navigate(["/letra-d"]);
+  }
+
+  anterior(){
+    this.router.navigate(["/letra-b"]);
+  }
+
+  public ocultar: boolean = false;
+  accion(){
+    this.ocultar = !this.ocultar;
+  }
 }
