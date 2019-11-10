4d21122b6909b1248ca5e88a51f86fcc9086d35c
Jessnangel <jessnangelsaballos@gmail.com>
2019-11-10 09:17:51 -0600

    refactore: Se modifico el t.page

--- src/app/letra-t/letra-t.page.ts
+++ src/app/letra-t/letra-t.page.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-letra-t',
   templateUrl: './letra-t.page.html',
   styleUrls: ['./letra-t.page.scss'],
 })
-export class LetraTPage implements OnInit {
+export class LetraTPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  home(){
+    this.router.navigate(["/categorias"]);
+  }
+
+  regresar_categoria(){
+    this.router.navigate(["/cat-abecedario"]);
+  }
+
+  siguiente(){
+    this.router.navigate(["/letra-u"]);
+  }
+
+  anterior(){
+    this.router.navigate(["/letra-s"]);
+  }
+
+  public ocultar: boolean = false;
+  accion(){
+    this.ocultar = !this.ocultar;
   }
 
 }
