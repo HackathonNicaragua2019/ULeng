84b65f0645c0b1a37bb520298c7ea2c09120d688
FernandoMZamora <zamoraf161@gmail.com>
2019-12-05 18:58:05 -0600

    refactore: Modificación de la categoría Departamentos

--- src/app/cat-departamentos/cat-departamentos.page.ts
+++ src/app/cat-departamentos/cat-departamentos.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cat-departamentos',
   templateUrl: './cat-departamentos.page.html',
   styleUrls: ['./cat-departamentos.page.scss'],
 })
-export class CatDepartamentosPage implements OnInit {
+export class CatDepartamentosPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  
+  regresar_categoria(){
+    this.router.navigate(["/categorias"]);
+  }
 
-  ngOnInit() {
+  home(){
+    this.router.navigate(["/home"]);
   }
 
 }
