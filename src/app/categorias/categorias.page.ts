b0761d936cfcb2090e1ea901f3b40ae6ef5ee2ad
FernandoMZamora <zamoraf161@gmail.com>
2019-12-05 19:14:57 -0600

    refactore: Modificación de Categorias

--- src/app/categorias/categorias.page.ts
+++ src/app/categorias/categorias.page.ts
@@ -14,4 +14,44 @@ export class CategoriasPage {
     this.router.navigate(["/home"]);
   }
 
+  goAbecedario(){
+    this.router.navigate(["/cat-abecedario"]);
+  }
+
+  goAnimales(){
+    this.router.navigate(["/cat-animales"]);
+  }
+
+  goBebidas(){
+    this.router.navigate(["/cat-bebidas"]);
+  }
+
+  goColores(){
+    this.router.navigate(["/cat-colores"]);
+  }
+
+  goComidas(){
+    this.router.navigate(["/cat-comidas"]);
+  }
+
+  goDepartamentos(){
+    this.router.navigate(["/cat-Departamentos"]);
+  }
+
+  goEmociones(){
+    this.router.navigate(["/cat-Emociones"]);
+  }
+
+  goFamilia(){
+    this.router.navigate(["/cat-familia"]);
+  }
+
+  goNumeros(){
+    this.router.navigate(["/cat-numeros"]);
+  }
+
+  goSaludos(){
+    this.router.navigate(["/cat-saludos"]);
+  }
+
 }
